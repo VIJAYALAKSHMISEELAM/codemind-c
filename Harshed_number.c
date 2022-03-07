1cd72d90506bbb0b287881cd2960be34efa7b367
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-03-07 09:36:58 +0530

    #include<stdio.h> int main() {     int a,sum=0,temp,d;     scanf("%d",&a);     temp=a;     while(a!=0)     {         d=a%10;         a=a/10;         sum=sum+d;     }     if(temp%sum==0)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Harshed_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,sum=0,temp,d;
+    scanf("%d",&a);
+    temp=a;
+    while(a!=0)
+    {
+        d=a%10;
+        a=a/10;
+        sum=sum+d;
+    }
+    if(temp%sum==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
