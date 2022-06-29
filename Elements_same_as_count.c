46883c88ac9416989cb522d05ac709c2a9259ddb
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-06-29 22:40:46 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],j,p,m=0,c;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         if(arr[i]!=-1)         {             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j] && i!=j)                 {                     c++;                     arr[j]=-1;
                    } 
                    
                } 
                
            } 
            if(c==arr[i])
            {
                m++;
                p=arr[i];
                printf("%d ",p);
            }
        }
        if(m==0) 
        {
            printf("-1");
        }
    
    }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],j,p,m=0,c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        if(arr[i]!=-1)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i!=j)
+                {
+                    c++;
+                    arr[j]=-1; 
+                    
+                } 
+                
+            } 
+            
+        } 
+        if(c==arr[i])
+        {
+            m++;
+            p=arr[i];
+            printf("%d ",p);
+        }
+    }
+    if(m==0) 
+    {
+        printf("-1");
+    }
+
+}
