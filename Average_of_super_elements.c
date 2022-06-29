4b038cb84714b14c6660390079d30bf64b870fbb
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-06-29 20:41:36 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],j,p,c;     float avg,m=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         if(arr[i]!=-1)         {             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j] && i!=j)                 {                     c++;                     arr[j]=-1;                 }             }         }         if(c==arr[i])         {             m++;             p+=arr[i];         }     }     if(m==0)     {         printf("-1");     }     else     {         avg=(float)p/m;         printf("%.2f",avg);     }
    }

---
+++ Average_of_super_elements.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],j,p,c;
+    float avg,m=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1; 
+        if(arr[i]!=-1)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i!=j)
+                {
+                    c++;
+                    arr[j]=-1;
+                }
+            }
+        }
+        if(c==arr[i])
+        {
+            m++;
+            p+=arr[i];
+        }
+    }
+    if(m==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        avg=(float)p/m;
+        printf("%.2f",avg);
+    }
+    
+}
